7470510a33da7d87eb8968c570cde71b013796fb
Wan Saleh <wansaleh@gmail.com>
2017-08-13 13:27:03 +0800

    Updates.

--- src/index.js
+++ src/index.js
@@ -9,35 +9,9 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, '..', 'db', 'db.json'))
 const middlewares = jsonServer.defaults()
 
-// Set default middlewares (logger, static, cors and no-cache)
 server.use(middlewares)
 
 server.use(cors({ origin: '*', credentials: true }));
-
-// server.use((req, res, next) => {
-//   const { date } = require(path.join(__dirname, 'last-updated.json'))
-
-//   fetch(`https://api.github.com/repos/${repo}/commits`, {
-//     headers: {
-//       'Authorization': `token ${token}`,
-//       'Time-Zone': 'Asia/Kuala_Lumpur'
-//     }
-//   })
-//   .then(res => res.json())
-//   .then(data => {
-//     if (Date.parse(data[0].commit.author.date) > date) {
-//       console.log('New commits. Updating db.json');
-//       fetcher(() => next())
-//     }
-//     else {
-//       console.log('No new commits.');
-//       next()
-//     }
-//   })
-//   .catch(err => {
-//     next()
-//   })
-// })
 
 server.get('/info', (req, res) => {
   fetch(`https://api.github.com/repos/${repo}`, {
